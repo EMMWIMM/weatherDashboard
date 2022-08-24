18cd054e3795c27e5c073f3290a8fbf62ef33ace
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-08-23 19:59:19 -0600

    got current day api pull working

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var requestURL = "http://api.openweathermap.org/data/2.5/weather?APPID=8cc2bc5b692b912633c29866f21e24d9";
+var requestURL = "http://api.openweathermap.org/data/2.5/weather?APPID=8cc2bc5b692b912633c29866f21e24d9&units=imperial";
 var todayWind = document.getElementById("todayWind");
 var todayUV = document.getElementById("todayUV");
 var todayHum = document.getElementById("todayHum");
@@ -14,18 +14,35 @@ var currentWind ="";
 var currentUV= "";
 // fetch api and what to do with retrieved info
 //const car = {type:"Fiat", model:"500", color:"white"};
-var weatherData = {temp:"90", wind:"something", humidity:"20", uvIndex:"12"} ;
+var weatherData = {temp:"90", wind:"something", humidity:"20", skies:"12"} ;
 function getAPICity(cityName){
   return fetch(requestURL+"&q="+cityName)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((response) => {
+      return response.json().then((data) => {
+        console.log(data);
+        return data;
+      }).catch((err) => {
+        console.log(err);
+      })
+    });
   // .then(){
   //   console.log("we got it bois");
   // }
 }
-function renderWeatherData(cityName){
-  var cityWeatherData = getAPICity(cityName);
-  weatherData = cityWeatherData;
+async function renderWeatherData(cityName){
+  var cityWeatherData = await getAPICity(cityName);
+  console.log("temp? "+cityWeatherData.main.temp);
+  weatherData.temp = cityWeatherData.main.temp;
+
+  console.log("wind? "+cityWeatherData.wind.speed);
+  weatherData.wind = cityWeatherData.wind.speed;
+
+  console.log("humidity? "+cityWeatherData.main.humidity);
+  weatherData.humidity = cityWeatherData.main.humidity;
+
+  console.log("temp? "+cityWeatherData.weather[0].main);
+  weatherData.skies = cityWeatherData.weather[0].main;
+  //weatherData = cityWeatherData;
 
 }
 
