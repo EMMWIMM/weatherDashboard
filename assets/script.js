8cdcf6fb782436955903e8d234d7f261f2c928f4
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-08-23 18:13:56 -0600

    added jsand tried api fetch

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=47abe5a03023354eae000f4a15ebd2e0";
+var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
 var todayWind = document.getElementById("todayWind");
 var todayUV = document.getElementById("todayUV");
 var todayHum = document.getElementById("todayHum");
@@ -12,15 +12,23 @@ var currentTemp = "";
 var currentHum = "";
 var currentWind ="";
 var currentUV= "";
-var atlantaURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.7490&lon=84.3880&exclude={part}&appid={API key}";
-var washingtonURL = "https://api.openweathermap.org/data/2.5/onecall?lat=38.9072&lon=77.0369&exclude={part}&appid={API key}";
-var portlandURL
-
-
+var atlantaURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.74&lon=84.38&appid=47abe5a03023354eae000f4a15ebd2e0";
+var washingtonURL = "https://api.openweathermap.org/data/2.5/onecall?lat=38.9072&lon=77.0369&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
+var portlandURL = "https://api.openweathermap.org/data/2.5/onecall?lat=45.5152&lon=122.6784&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
+var saltLakeCityURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7608&lon=111.8910&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
+var orlandoURL = "https://api.openweathermap.org/data/2.5/onecall?lat=28.5384&lon=81.3789&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
+var newYorkURL = "https://api.openweathermap.org/data/2.5/onecall?lat=40.7128&lon=74.0060&exclude=minutely&appid=47abe5a03023354eae000f4a15ebd2e0";
+var atlantaTest = "api.openweathermap.org/data/2.5/onecall?lat=33.74&lon=84.38&callback=47abe5a03023354eae000f4a15ebd2e0"
 // fetch api and what to do with retrieved info
-function getAPI(){
-  fetch(requestUrl)
-  .then()
+function getAPIAtlanta(){
+  fetch(atlantaURL)
+  .then((response) => response.json())
+ .then((data) => console.log(data));
+  // .then(){
+  //   console.log("we got it bois");
+  // }
 }
+
+
 
 // use api to set textcontent on required feilds
